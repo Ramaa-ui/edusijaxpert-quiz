d91755f480201a08e49cd2485aecd60545b3d8aa
Ramaa-ui <shafaoctaza07@gmail.com>
2025-07-22 13:11:00 +0700

    Tambah API

---
+++ app/api/route.js
@@ -0,0 +1,11 @@
+// app/api/cekjawaban/route.js
+export async function GET(request) {
+  const angka = request.nextUrl.searchParams.get("angka");
+  const benar = request.nextUrl.searchParams.get("benar");
+
+  if (angka === benar) {
+    return new Response("Benar!");
+  } else {
+    return new Response("Salah!");
+  }
+}
